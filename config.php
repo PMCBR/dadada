b6c170d5a825ec80de72df4bbbcd86ebee297fa3
PMCBR <cesargamer1980.22010@gmail.com>
2026-05-11 20:38:26 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'ventrix.fo best drops Asstin');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'asstin_316');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '7370925341');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'Kravenchk_bot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '8575854700:AAHRSpwuFu0BCYN5FI9hXG_meoAJMDSLuJA');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-5189870315');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
