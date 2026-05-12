a69b14fa4fb1ad4dc62011759a6483f09ab60fba
PMCBR <cesargamer1980.22010@gmail.com>
2026-05-11 20:56:52 -0400

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '8575854700
 if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '');
 if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-5189870315');
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
-if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'railway');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'mysql.railway.internal');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'root');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'fIyBbCXsfrIXqGpMkQnLtvEQqYPRaUOU');
